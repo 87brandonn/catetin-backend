71487c232f3dd7e178d696391645698129d3071f
vincent.tanjaya <vincent.tanjaya@192.168.1.9>
2022-02-13 15:33:02 +0700

    login and mysql connection

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import { getAuth } from "../handlers/auth";
-
+import controller from '../controllers/user';
+import bodyParser from 'body-parser';
 const router = express.Router();
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
 router.get("/", getAuth);
+
+
+router.get('/validate', controller.validateToken);
+router.post('/register', controller.register);
+router.post('/login', controller.login);
+router.get('/get/all', controller.getAllUsers);
 
 export default router;
--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import router from "./auth";
 import authRouter from "./auth";
+import controller from '../controllers/user';
 
 const routes = express.Router();
 
 routes.use("/auth", authRouter);
+router.get('/validate', controller.validateToken);
+router.post('/register', controller.register);
+router.post('/login', controller.login);
+router.get('/get/all', controller.getAllUsers);
 
 export default routes;
