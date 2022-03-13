96e05e2cef842c8f2147134bcc93c516fa8d98cd
Vincent Tanjaya <vincenttanjaya@192.168.1.6>
2022-03-13 19:44:25 +0700

    transaksi

--- routes/index.ts
+++ routes/index.ts
@@ -3,6 +3,7 @@ import router from "./auth";
 import authRouter from "./auth";
 import controller from "../controllers/user";
 import controllerBarang from "../controllers/barang";
+import controllerTransaksi from "../controllers/transaksi"
 import extractJWT from "../middleware/extractJWT";
 
 const routes = express.Router();
@@ -13,5 +14,6 @@ routes.post("/update/profile", extractJWT, controller.updateProfile)
 routes.post("/insert/barang", extractJWT, controllerBarang.insertBarang)
 routes.post("/update/barang", extractJWT, controllerBarang.updateBarang)
 routes.get("/get/barang", extractJWT, controllerBarang.getListBarang)
+routes.post("/insert/transaksi", extractJWT, controllerTransaksi.insertTransaksi)
 
 export default routes;
