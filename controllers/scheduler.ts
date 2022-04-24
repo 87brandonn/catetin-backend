ea2bc2dd5d64be6b8855a5474b83e017c77ab1b0
87brandonn <53943296+87brandonn@users.noreply.github.com>
2022-04-24 17:01:39 +0700

    checkpoint replacing old jobs

--- controllers/scheduler.ts
+++ controllers/scheduler.ts
@@ -66,14 +66,7 @@ const addScheduler = async (req: Request, res: Response) => {
     userData = JSON.parse(JSON.stringify(userData));
 
     const jobIndex = jobs.findIndex((job) => job.id === parseInt(user_id, 10));
-
-    if (jobIndex !== -1) {
-      jobs.splice(jobIndex, 1);
-    }
-
     const schedule = JSON.parse(JSON.stringify(data));
-
-    console.log(schedule);
 
     console.log(
       `${getCronTime(schedule.minute, schedule.minute, "0")} ${getCronTime(
@@ -91,39 +84,75 @@ const addScheduler = async (req: Request, res: Response) => {
       )}`
     );
 
-    jobs.push({
-      id: parseInt(user_id, 10),
-      initDate: currentDate.toISOString(),
-      job: new CronJob(
-        `${getCronTime(schedule.minute, schedule.minute, "0")} ${getCronTime(
-          schedule.hour,
-          schedule.hour,
-          "0"
-        )} ${getCronTime(
-          schedule.dayOfMonth,
-          schedule.dayOfMonth,
-          getCronTime(schedule.month, 1, "*")
-        )} ${getCronTime(schedule.month, schedule.month, "*")} ${getCronTime(
-          schedule.dayOfWeek,
-          schedule.dayOfWeek,
-          "*"
-        )}`,
-        async () => {
-          try {
-            await triggerCron(
-              userData.id,
-              userData.email,
-              userData.Profile?.storeName
-            );
-          } catch (err) {
-            console.error(err);
-          }
-        },
-        null,
-        true,
-        "Asia/Jakarta"
-      ),
-    });
+    if (jobIndex !== -1) {
+      jobs.splice(jobIndex, 1, {
+        id: parseInt(user_id, 10),
+        initDate: currentDate.toISOString(),
+        job: new CronJob(
+          `${getCronTime(schedule.minute, schedule.minute, "0")} ${getCronTime(
+            schedule.hour,
+            schedule.hour,
+            "0"
+          )} ${getCronTime(
+            schedule.dayOfMonth,
+            schedule.dayOfMonth,
+            getCronTime(schedule.month, 1, "*")
+          )} ${getCronTime(schedule.month, schedule.month, "*")} ${getCronTime(
+            schedule.dayOfWeek,
+            schedule.dayOfWeek,
+            "*"
+          )}`,
+          async () => {
+            try {
+              await triggerCron(
+                userData.id,
+                userData.email,
+                userData.Profile?.storeName
+              );
+            } catch (err) {
+              console.error(err);
+            }
+          },
+          null,
+          true,
+          "Asia/Jakarta"
+        ),
+      });
+    } else {
+      jobs.push({
+        id: parseInt(user_id, 10),
+        initDate: currentDate.toISOString(),
+        job: new CronJob(
+          `${getCronTime(schedule.minute, schedule.minute, "0")} ${getCronTime(
+            schedule.hour,
+            schedule.hour,
+            "0"
+          )} ${getCronTime(
+            schedule.dayOfMonth,
+            schedule.dayOfMonth,
+            getCronTime(schedule.month, 1, "*")
+          )} ${getCronTime(schedule.month, schedule.month, "*")} ${getCronTime(
+            schedule.dayOfWeek,
+            schedule.dayOfWeek,
+            "*"
+          )}`,
+          async () => {
+            try {
+              await triggerCron(
+                userData.id,
+                userData.email,
+                userData.Profile?.storeName
+              );
+            } catch (err) {
+              console.error(err);
+            }
+          },
+          null,
+          true,
+          "Asia/Jakarta"
+        ),
+      });
+    }
 
     res.status(200).send({
       data,
