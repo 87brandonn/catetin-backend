32eec3e3b6ec04bdca97229f1e5717bb9a151ec8
87brandonn <53943296+87brandonn@users.noreply.github.com>
2022-05-05 17:18:39 +0700

    adjust get barang detail

--- controllers/barang.ts
+++ controllers/barang.ts
@@ -207,6 +207,12 @@ const getListBarang = async (
           ) === -1;
         return isNotIncludeTransaction;
       });
+      data = data.map((item) => ({
+        ...item,
+        Transactions: item.Transactions.filter(
+          (transaction) => !transaction.deleted
+        ),
+      }));
     }
 
     res.status(200).send({
